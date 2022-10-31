819673c45eb0c607ab61577d25710acefd01e9ac
Edmund Adomako <edmund.adomako@amalitech.com>
2022-10-31 09:59:01 +0000

    First commit

---
+++ AWS Lambda/src/main/java/com/appsdeveloperblog/aws/lambda/AegisRiderApplication.java
@@ -0,0 +1,11 @@
+package com.appsdeveloperblog.aws.lambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AegisRiderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AegisRiderApplication.class, args);
+    }
+}
